e9e95229dd7a0ee35eac079afa2046954bafd4b6
Jason Miller <jmillerlaureate@gmail.com>
2014-10-28 11:44:46 -0400

    initial commit

---
+++ R/source2_function.R
@@ -0,0 +1,5 @@
+source2 <- function(file, start, end, ...) {
+  # Sources just part of a file
+  file.lines <- scan(file, what=character(), skip=start-1, nlines=end-start+1, sep='\n')
+  file.lines.collapsed <- paste(file.lines, collapse='\n')
+  source(textConnection(file.lines.collapsed), ...)}
